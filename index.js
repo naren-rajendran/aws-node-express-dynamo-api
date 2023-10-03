1f85337941d49cdc817709a2336c3646b16a117b
naren-rajendran <rnarendran@live.com>
2023-10-03 08:51:12 -0700

    cleanup

--- index.js
+++ index.js
@@ -2,7 +2,3 @@ const serverless = require("serverless-http");
 const app = require("./app");
 
 module.exports.handler = serverless(app);
-
-// app.listen(3000, () => {
-//     console.log('express app is running...');
-// });
